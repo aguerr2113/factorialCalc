f7f5992af4d30b7cbc04f7ad31888cbd729f4d63
Art Guerrero <artemio.guerreru@g.austincc.edu>
2025-03-18 09:27:01 -0500

    renamed the java class to suit the project,imported Scanner for user input, implemented a skeleton for a recursive method

---
+++ FactorialCalculator.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class FactorialCalculator {
+
+    public static void main(String[] args) {
+        
+    }
+
+    public static int factorial(int n){
+
+    }
+
+}
